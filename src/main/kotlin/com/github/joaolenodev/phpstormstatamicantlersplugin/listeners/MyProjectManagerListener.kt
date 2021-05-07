14e57f4168d0dbad0fb1e97792df2e8de1b82c44
GitHub Action <action@github.com>
2021-05-07 06:40:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/joaolenodev/phpstormstatamicantlersplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/joaolenodev/phpstormstatamicantlersplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.joaolenodev.phpstormstatamicantlersplugin.listeners
 
+import com.github.joaolenodev.phpstormstatamicantlersplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/joaolenodev/phpstormstatamicantlersplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.joaolenodev.phpstormstatamicantlersplugin.services
+
+import com.github.joaolenodev.phpstormstatamicantlersplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
